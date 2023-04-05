7c40b7bea4fe2ac154f52b6d8b170e90580e52f6
parkyes90 <parkyes90@gmail.com>
2023-04-05 17:25:58 +0900

    Add LoginPage

--- services/frontends/container/src/App.tsx
+++ services/frontends/container/src/App.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import LoginPage from "./pages/login-page";
+import styled from "@emotion/styled";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+`;
 
 function App() {
-  return <div>App</div>;
+  return (
+    <Container>
+      <LoginPage />
+    </Container>
+  );
 }
 
 export default App;
