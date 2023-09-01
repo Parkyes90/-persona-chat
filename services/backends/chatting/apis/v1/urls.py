5ac9e0c4066c146eb8c8c83d9aadb5b388356cd9
parkyes90 <parkyes90@gmail.com>
2023-09-01 15:53:58 +0900

    initialize

--- services/backends/chatting/apis/v1/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import include
-from django.urls import path
-
-app_name = "v1"
-urlpatterns = [
-    path("users/", include("apis.v1.users.urls")),
-]
--- services/backends/chatting/apis/v1/users/urls.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.routers import DefaultRouter
-
-from .views import UserAPIViewSet
-
-app_name = "users"
-router = DefaultRouter()
-router.register("", UserAPIViewSet, basename="users")
-urlpatterns = router.urls
